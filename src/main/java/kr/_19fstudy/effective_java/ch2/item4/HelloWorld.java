fc293e547ff1039dd8f5bd8c4205c2c239cf48f7
doubleknd26 <doubleknd26@gmail.com>
2019-03-03 13:50:11 +0900

    [add] helloworld source code

---
+++ src/main/java/kr/_19fstudy/effective_java/ch2/item4/HelloWorld.java
@@ -0,0 +1,11 @@
+package kr._19fstudy.effective_java.ch2.item4;
+
+/**
+ * Created by Kideok Kim on 2019-03-03.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
