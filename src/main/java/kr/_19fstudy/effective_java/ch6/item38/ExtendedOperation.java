cb4480c0c19ba08a0278e656cb366d154ecf5c30
bigsky-park <ptm302@gmail.com>
2019-04-01 19:51:40 +0900

    [item38] Add study material

---
+++ src/main/java/kr/_19fstudy/effective_java/ch6/item38/ExtendedOperation.java
@@ -0,0 +1,29 @@
+package kr._19fstudy.effective_java.ch6.item38;
+
+public enum ExtendedOperation implements Operation {
+    EXP("^") {
+        @Override
+        public double apply(double x, double y) {
+            return Math.pow(x, y);
+        }
+    },
+    REMAINDER("%") {
+        @Override
+        public double apply(double x, double y) {
+            return x % y;
+        }
+    };
+
+    private final String symbol;
+
+    ExtendedOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return this.symbol;
+
+
+    }
+}
---
+++ src/main/java/kr/_19fstudy/effective_java/ch6/item38/Operation.java
@@ -0,0 +1,7 @@
+package kr._19fstudy.effective_java.ch6.item38;
+
+public interface Operation {
+
+    double apply(double x, double y);
+
+}
